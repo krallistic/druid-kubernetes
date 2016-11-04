ef6bb6171000aaf1ee1d94135e900ed374a8e6ad
Jakob Karalus <jakobkaralus@Jakobs-MacBook-Pro.local>
2016-11-04 14:18:29 +0100

    Coordinate image build

--- build_images.sh
+++ build_images.sh
@@ -4,3 +4,4 @@ TAG="latest"
 docker build -t jkaralus/druid8s-base:$TAG base
 docker build -t jkaralus/druid8s-quickstart:$TAG quickstart
 docker build -t jkaralus/druid8s-local-zk:$TAG local-zk
+docker build -t jkaralus/druid8s-coordinator:$TAG coordinator
